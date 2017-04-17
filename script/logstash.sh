675d1d56932258a34e25f7384d880f5b55790901
siriaodao <siriaodao@vip.qq.com>
2017-04-17 11:21:57 +0800

    update logstash script

--- script/logstash.sh
+++ script/logstash.sh
@@ -2,4 +2,6 @@
 export JAVA_HOME=/opt/product/tools/jdk1.8.0_121
 export PATH=$JAVA_HOME/bin:$PATH
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8  -Duser.timezone=GMT+08"
+rm -rf /opt/product/logstash/data
+ln -s  /opt/product/data/elk/logstashdata /opt/product/logstash/data
 /opt/product/logstash/bin/logstash -f /opt/product/logstash/config/logstash.conf
